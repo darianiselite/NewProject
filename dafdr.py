1565a8f8a1ad7992ef32eef69875a04f3f3a8476
darianiselite <33328056+darianiselite@users.noreply.github.com>
2017-11-06 13:37:16 -0500

    Add files via upload

---
+++ dafdr.py
@@ -0,0 +1,32 @@
+import turtle
+turtle.colormode(255)#brings the color mode function
+
+
+bob=turtle.Turtle()
+bob.width(5)
+bob.speed(9000)
+turtle.bgcolor(255,100,89)
+            
+
+
+for times in range (255):
+   bob.color(times,100,255)
+   bob.forward(times*5+10)
+   bob.right(980)
+
+
+   darian= turtle.Turtle()
+
+bob=turtle.Turtle()
+bob.width(5)
+bob.speed(700)
+turtle.bgcolor(255,100,100)
+            
+
+
+for times in range (255):
+   bob.color(times,155,255)
+   bob.forward(times*2+10)
+   bob.right(98)
+
+   darian= turtle.Turtle()
